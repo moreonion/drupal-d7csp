da145d50fb02db9d3260a3719050285ee9cc8a44
Roman Zimmermann <roman@more-onion.com>
2025-04-02 11:22:38 +0200

    fix(wysiwyg): Always add admin and default theme

--- modules/wysiwyg.php
+++ modules/wysiwyg.php
@@ -13,9 +13,12 @@ function wysiwyg_d7csp_hosts() {
   // Needed for ckeditor 4.
   $hosts['script-src-attr'][] = "'unsafe-inline'";
   $themes = list_themes();
-  $css_themes = array_unique(array_filter(array_map(function ($profile)  {
+  $css_themes = array_map(function ($profile)  {
     return $profile->settings['css_theme'] ?? '';
-  }, wysiwyg_profile_load_all())));
+  }, wysiwyg_profile_load_all());
+  $css_themes[] = variable_get('admin_theme');
+  $css_themes[] = variable_get('theme_default', 'bartik');
+  $css_themes = array_unique(array_filter($css_themes));
   foreach ($css_themes as $theme_name) {
     $theme = $themes[$theme_name];
     if (file_exists($template_php = dirname($theme->filename) . '/template.php')) {
