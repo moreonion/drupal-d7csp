595109915892ff17d8b1f5c50ead6fd95105f27b
Roman Zimmermann <roman@more-onion.com>
2025-04-02 11:01:31 +0200

    feat(wysiwyg): Simplify by iterating the wysiwyg themes directly

--- modules/wysiwyg.php
+++ modules/wysiwyg.php
@@ -12,13 +12,11 @@ function wysiwyg_d7csp_hosts() {
   $hosts = [];
   // Needed for ckeditor 4.
   $hosts['script-src-attr'][] = "'unsafe-inline'";
-  wysiwyg_initialize_cache();
-  if (!($cache = cache_get('wysiwyg_css'))) {
-    // No wysiwyg profile was configured to use a css theme.
-    return $hosts;
-  }
   $themes = list_themes();
-  foreach (array_keys($cache->data) as $theme_name) {
+  $css_themes = array_unique(array_filter(array_map(function ($profile)  {
+    return $profile->settings['css_theme'] ?? '';
+  }, wysiwyg_profile_load_all())));
+  foreach ($css_themes as $theme_name) {
     $theme = $themes[$theme_name];
     if ($theme->engine == 'phptemplate') {
       require_once dirname($theme->filename) . '/template.php';
@@ -29,21 +27,4 @@ function wysiwyg_d7csp_hosts() {
     }
   }
   return $hosts;
-}
-
-/**
- * Helper function to initialize the CSS cache.
- */
-function wysiwyg_initialize_cache() {
-  $themes = array_unique(array_filter(array_map(function ($profile)  {
-    return $profile->settings['css_theme'] ?? '';
-  }, wysiwyg_profile_load_all())));
-  foreach ($themes as $theme) {
-    $css = wysiwyg_get_css($theme);
-    if (count($css) === 1 && $url = $css[0]) {
-      if (strpos(parse_url($url, PHP_URL_PATH), '/wysiwyg_theme/') === 0) {
-        drupal_http_request($url);
-      }
-    }
-  }
 }
