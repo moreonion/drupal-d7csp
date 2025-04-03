37d2c4098c68d32c51e4edc5633eadf70f130f0a
Roman Zimmermann <roman@more-onion.com>
2025-04-03 13:19:02 +0200

    feat(wysiwyg): Pretend theme is active when calling its alter-hook

--- modules/wysiwyg.php
+++ modules/wysiwyg.php
@@ -19,15 +19,18 @@ function wysiwyg_d7csp_hosts() {
   $css_themes[] = variable_get('admin_theme');
   $css_themes[] = variable_get('theme_default', 'bartik');
   $css_themes = array_unique(array_filter($css_themes));
+  $backup_active_theme = $GLOBALS['theme_key'];
   foreach ($css_themes as $theme_name) {
     $theme = $themes[$theme_name];
     if (file_exists($template_php = dirname($theme->filename) . '/template.php')) {
       require_once $template_php;
       $alter_fn = "{$theme->name}_d7csp_hosts_alter";
       if (function_exists($alter_fn)) {
+        $GLOBALS['theme_key'] = $theme;
         $alter_fn($hosts);
       }
     }
   }
+  $GLOBALS['theme_key'] = $backup_active_theme;
   return $hosts;
 }
