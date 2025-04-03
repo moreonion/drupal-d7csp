19ba90fefa48ed4ea2a5717a520cd79a84808293
Roman Zimmermann <roman@more-onion.com>
2025-04-03 13:33:51 +0200

    feat(wysiwyg): Support calling of base-themes

--- modules/wysiwyg.php
+++ modules/wysiwyg.php
@@ -6,6 +6,21 @@
  */
 
 /**
+ * Get base tthe
+ * @param mixed $theme
+ * @return void
+ */
+function _d7csp_get_base_themes(string $theme): array {
+  $themes = list_themes();
+  $result = [$theme];
+  while ($base_theme = $themes[$theme]->base_theme ?? NULL) {
+    $theme = $base_theme;
+    $result[] = $theme;
+  }
+  return array_reverse($result);
+}
+
+/**
  * Implements hook_d7csp_hosts().
  */
 function wysiwyg_d7csp_hosts() {
@@ -21,13 +36,15 @@ function wysiwyg_d7csp_hosts() {
   $css_themes = array_unique(array_filter($css_themes));
   $backup_active_theme = $GLOBALS['theme_key'];
   foreach ($css_themes as $theme_name) {
-    $theme = $themes[$theme_name];
-    if (file_exists($template_php = dirname($theme->filename) . '/template.php')) {
-      require_once $template_php;
-      $alter_fn = "{$theme->name}_d7csp_hosts_alter";
-      if (function_exists($alter_fn)) {
-        $GLOBALS['theme_key'] = $theme;
-        $alter_fn($hosts);
+    $GLOBALS['theme_key'] = $theme_name;
+    foreach (_d7csp_get_base_themes($theme_name) as $base_theme) {
+      $theme = $themes[$base_theme];
+      if (file_exists($template_php = dirname($theme->filename) . '/template.php')) {
+        require_once $template_php;
+        $alter_fn = "{$theme->name}_d7csp_hosts_alter";
+        if (function_exists($alter_fn)) {
+          $alter_fn($hosts);
+        }
       }
     }
   }
